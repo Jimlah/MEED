b6e5f7e2d71f0307033cc794e16b0e06a7c1d436
Abdullahi <abdullahij951@gmail.com>
2021-05-16 08:57:16 +0100

    chore: created charts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,26 +4,28 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use ConsoleTVs\Charts\Registrar as Charts;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-      //  
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot(Charts $charts)
+  {
+    $charts->register([
+      \App\Charts\SampleChart::class
+    ]);
+  }
 }
--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.dashboard')
 
 @section('body')
+<div class="grid w-full h-full grid-cols-2 gap-5">
+  <div class="p-5 bg-white md:col-span-1 col-span-full bg-opacity-90">
+    <div id="chart" class="h-full"></div>
+  </div>
+  <div class="hidden p-5 bg-white md:col-span-1 md:block bg-opacity-90">
 
+    <livewire:calender>
+
+  </div>
+</div>
+<script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+<!-- Chartisan -->
+<script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+<!-- Your application script -->
+<script>
+  const chart = new Chartisan({
+    el: '#chart',
+    url: "{{ route('charts.samplechart') }}",
+  });
+</script>
 @endsection
