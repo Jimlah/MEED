0372693fea6aa406b3d8145cbb17b613386d34d3
Abdullahi <abdullahij951@gmail.com>
2021-05-18 06:36:19 +0100

    run production to host websitte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ env("APP_NAME") }}</title>
-  <style>
-    * {
-      margin: 0;
-      padding:0;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }}</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+
+    </style>
 </head>
-<body class="w-full h-screen " >
-  <div class="w-full h-screen bg-center bg-no-repeat bg-contain"  style="background-image: url('{{ asset("img/hm.jpg") }}')">
-    <span class="text-white">Hey</span>
-  </div>
+
+<body class="w-full h-screen ">
+    <div class="flex items-center justify-center w-full h-screen gap-6 text-white bg-center bg-no-repeat bg-cover"
+        style="background-image: url('{{ asset('img/hm.jpg') }}')">
+        <a href="{{ route('showlogin') }}" class="px-3 py-2 text-5xl font-bold bg-blue-500 rounded hover:bg-opacity-50">Login</a>
+        <a href="{{ route('showregister') }}" class="px-3 py-2 text-5xl font-bold bg-red-500 rounded hover:bg-opacity-50">Register</a>
+    </div>
 </body>
+
 </html>
